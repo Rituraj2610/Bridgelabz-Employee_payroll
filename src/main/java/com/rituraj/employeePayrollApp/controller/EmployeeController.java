a985100c2e965bab67abb3563c1d2ee9782e4f07
Rituraj2610 <26rituraj@gmail.com>
2025-04-19 15:30:49 +0530

    [Rituraj] ADD . UC1

--- src/main/java/com/rituraj/employeePayrollApp/controller/EmployeeController.java
+++ src/main/java/com/rituraj/employeePayrollApp/controller/EmployeeController.java
@@ -1,12 +1,40 @@
 package com.rituraj.employeePayrollApp.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.rituraj.employeePayrollApp.dto.EmployeePostDTO;
+import com.rituraj.employeePayrollApp.dto.SalaryUpdateByIdDTO;
+import com.rituraj.employeePayrollApp.model.EmployeePayroll;
+import com.rituraj.employeePayrollApp.service.EmployeeService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("employeepayroll")
+@RequestMapping("api/v1/employeepayroll")
 public class EmployeeController {
 
-    @GetMapping()
+    private EmployeeService employeeService;
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping("/employee")
+    public List<EmployeePayroll> getAllEmployees(){
+        return employeeService.getAllEmployees();
+    }
+
+    @PostMapping("/employee")
+    public String addEmployees(@RequestBody EmployeePostDTO employeePostDTO){
+        return employeeService.addEmployees(employeePostDTO);
+    }
+
+    @PutMapping("/salary")
+    public String updateEmployeeSalary(@RequestBody SalaryUpdateByIdDTO salaryUpdateByIdDTO){
+        return employeeService.updateEmployeeSalary(salaryUpdateByIdDTO);
+    }
+
+    @DeleteMapping("/employee")
+    public String deleteEmployee(@RequestParam int id){
+        return employeeService.deleteEmployee(id);
+    }
+
 }
--- src/main/java/com/rituraj/employeePayrollApp/repo/EmployeeRepo.java
+++ src/main/java/com/rituraj/employeePayrollApp/repo/EmployeeRepo.java
@@ -1,4 +1,9 @@
 package com.rituraj.employeePayrollApp.repo;
 
-public class EmployeeRepo {
+import com.rituraj.employeePayrollApp.model.EmployeePayroll;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepo extends JpaRepository<EmployeePayroll, Integer> {
 }
